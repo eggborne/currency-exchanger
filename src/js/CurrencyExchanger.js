53fcf779a82443ee52c31eb2f5e318a5334f72e8
Mike Donovan <mike@mikedonovan.dev>
2023-02-11 00:23:45 -0800

    Enable dropdown lists to be populated by API

--- src/js/CurrencyExchanger.js
+++ src/js/CurrencyExchanger.js
@@ -6,6 +6,13 @@ export default class CurrencyExchanger {
 
     window.addEventListener('load', async () => {
       await this.buildCachedData();
+      document.getElementById('base-currency-input').addEventListener('change', e => { 
+        document.getElementById('base-amount-input').placeholder = `Enter ${e.target.value}...`;
+      });
+      document.getElementById('target-currency-input').addEventListener('change', e => {
+        let currencyName = this.cachedEntryForRegion(e.target.value).currencyName;
+        document.querySelector('#output-area > label').innerHTML = `${currencyName}`;
+      });
       document.getElementById('exchange-form').addEventListener('submit', async e => {
         e.preventDefault();
         let baseAmount = document.getElementById('base-amount-input').value;
@@ -45,7 +52,8 @@ export default class CurrencyExchanger {
   }
 
   async buildCachedData() {
-    await this.cacheRegionInfo();
+    let regionInfo = await this.cacheRegionInfo();
+    this.buildRegionDropdowns(regionInfo);
     await this.cacheAllExchangeRatesForRegion(['USD']);
   }
 
@@ -80,6 +88,7 @@ export default class CurrencyExchanger {
         item.exchangeRates[targetCode] = exchangeRate;
       }
     });
+    this.cachedData = cachedData;
     return exchangeRate;
   }
 
@@ -92,5 +101,22 @@ export default class CurrencyExchanger {
       }
     });
     sessionStorage.setItem('currency_exchange_data', JSON.stringify(cachedData));
+  }
+
+  buildRegionDropdowns(regionData) {
+    let baseElement = document.getElementById('base-currency-input');
+    let targetElement = document.getElementById('target-currency-input');
+    for (const regionObj of regionData) {
+      let baseOption = document.createElement('option');
+      let targetOption = document.createElement('option');
+      baseOption.name = 'base-currency-input';
+      targetOption.name = 'target-currency-input';
+      baseOption.value = targetOption.value = regionObj.code;
+      baseOption.innerHTML = targetOption.innerHTML = `${regionObj.code} - ${regionObj.currencyName}`;
+      baseElement.append(baseOption);
+      targetElement.append(targetOption);
+    }
+    baseElement.value = 'USD';
+    targetElement.value = 'JPY';
   }
 }
